46b78a9d240bb8e67b8d4dc5a82988f3f3477604
Youtpout <EddyBoughioul@hotmail.fr>
2024-06-04 12:27:40 +0200

    need to supply token on create pair

--- contracts/src/Pool.ts
+++ contracts/src/Pool.ts
@@ -41,17 +41,55 @@ export class Pool extends TokenContract {
     this.checkZeroBalanceChange(forest);
   }
 
-  @method async createPair(_token0: PublicKey, _token1: PublicKey) {
+  @method.returns(UInt64)
+  async createPair(_token0: PublicKey, _token1: PublicKey, _amount0: UInt64, _amount1: UInt64) {
+    _token0.equals(_token1).assertFalse("Identical token");
+    _amount0.assertGreaterThan(UInt64.zero, "Insufficient amount 0");
+    _amount1.assertGreaterThan(UInt64.zero, "Insufficient amount 1");
+
     let _poolState = this.poolState.getAndRequireEquals();
     _poolState.init.assertFalse("Pool already created");
-
-    _token0.equals(_token1).assertFalse("Identical token");
 
     _poolState.init = Bool(true);
 
     this.poolState.set(_poolState);
     this.token0.set(_token0);
     this.token1.set(_token1);
+
+    let senderPublicKey = this.sender.getUnconstrained();
+    let senderUpdate = AccountUpdate.createSigned(senderPublicKey);
+    // senderUpdate.send({ to: this, amount: _amount0 });
+    let simpleToken0 = new SimpleToken(_token0);
+    simpleToken0.transfer(senderUpdate, this.address, _amount0);
+
+    let simpleToken1 = new SimpleToken(_token1);
+    simpleToken1.transfer(senderUpdate, this.address, _amount1);
+
+    let liquidity = UInt64.zero;
+
+    // use field for sqrt
+    let field0 = new Field(_amount0.toBigInt());
+    let field1 = new Field(_amount1.toBigInt());
+
+    let liquidityField = field0.mul(field1).sqrt().sub(minimunLiquidity);
+    liquidityField.assertGreaterThan(0, "Insufficient liquidity for minimun liquidity");
+    liquidity = UInt64.fromFields([liquidityField]);
+    liquidity.assertGreaterThan(UInt64.zero, "Insufficient liquidity");
+
+    // mint minimun to address 0
+    this.internal.mint({ address: PublicKey.empty(), amount: minimunLiquidity });
+    _poolState.totalSupply.add(minimunLiquidity);
+
+    // mint to user
+    this.internal.mint({ address: senderPublicKey, amount: liquidity });
+    _poolState.totalSupply.add(liquidity);
+
+    _poolState.reserve0.add(_amount0);
+    _poolState.reserve1.add(_amount1);
+
+    this.poolState.set(_poolState);
+
+    return liquidity;
   }
 
   @method.returns(UInt64)
@@ -70,23 +108,7 @@ export class Pool extends TokenContract {
     let simpleToken1 = new SimpleToken(address1);
     simpleToken1.transfer(senderUpdate, this.address, _amount1);
 
-    let liquidity = UInt64.zero;
-
-    if (_poolState.totalSupply.equals(new UInt64(0))) {
-      // use field for sqrt
-      let field0 = new Field(_amount0.toBigInt());
-      let field1 = new Field(_amount1.toBigInt());
-
-      let liquidityField = field0.mul(field1).sqrt().sub(minimunLiquidity);
-      liquidityField.assertGreaterThan(0, "Insufficient liquidity for minimun liquidity");
-
-      liquidity = UInt64.fromFields([liquidityField]);
-      // mint minimun to address 0
-      this.internal.mint({ address: PublicKey.empty(), amount: minimunLiquidity });
-    } else {
-      liquidity = _amount0.mul(_poolState.totalSupply).div(_poolState.reserve0)
-    }
-
+    let liquidity = _amount0.mul(_poolState.totalSupply).div(_poolState.reserve0)
     liquidity.assertGreaterThan(UInt64.zero, "Insufficient liquidity");
 
     // mint to user
@@ -94,10 +116,12 @@ export class Pool extends TokenContract {
 
     _poolState.reserve0.add(_amount0);
     _poolState.reserve1.add(_amount1);
+    _poolState.totalSupply.add(liquidity);
 
     this.poolState.set(_poolState);
 
-    return new UInt64(0);
+
+    return liquidity;
   }
 
 
